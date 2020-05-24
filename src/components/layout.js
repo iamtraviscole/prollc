7cf2124852b421be093f5323a048128221b66c10
Travis Cole <iamtraviscole@gmail.com>
2020-05-24 03:34:49 -0400

    remove gatsby boilerplate and customize

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,24 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React from 'react'
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import Header from './header'
+import '../styles/layout.scss'
 
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+const Layout = (props) => {
+  const subheader = props.pageTitle && (
+    <div className='layout__subheader'>
+      <h1>{props.pageTitle}</h1>
+    </div>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  return (
+    <div className='layout'>
+      <Header />
+      {subheader}
+      <main className='layout__main'>{props.children}</main>
+      <footer className='layout__footer'>
+        © {new Date().getFullYear()}
+      </footer>
+    </div>
+  )
 }
 
 export default Layout
