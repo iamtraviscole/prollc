4fc708fd60dc9312d9a1e0927dd82b9a6c664ed4
Travis Cole <iamtraviscole@gmail.com>
2020-05-26 04:31:34 -0400

    create placeholder components for plan forms

---
+++ src/components/plans/payment.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Payment = (props) => {
+  return (
+    <div>Payment</div>
+  )
+}
+
+export default Payment
---
+++ src/components/plans/registeredAgentDetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RegisteredAgentDetails = (props) => {
+  return (
+    <div>Registered Agent Details</div>
+  )
+}
+
+export default RegisteredAgentDetails
