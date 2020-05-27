183e045958cca4e745e200b574669126363c58f3
Travis Cole <iamtraviscole@gmail.com>
2020-05-27 04:09:59 -0400

    build out contactDetails component

--- src/components/plans/contactDetails.js
+++ src/components/plans/contactDetails.js
@@ -1,8 +1,59 @@
 import React from 'react'
 
+import StepHeader from './stepHeader'
+
+import '../../styles/plans/contactDetails.scss'
+
 const ContactDetails = (props) => {
   return (
-    <div>Contact Details</div>
+    <div className='contactDetails'>
+      <StepHeader title='Contact Details' />
+      <div className='contactDetails__input-ctr'>
+        <label htmlFor='contactDetails__input-first'>First Name *</label>
+        <input
+          type='text'
+          id='contactDetails__input-first'
+          name='contactDetails.firstName'
+          onChange={props.handleChange}
+        />
+      </div>
+      <div className='contactDetails__input-ctr'>
+        <label htmlFor='contactDetails__input-middle'>Second Name</label>
+        <input
+          type='text'
+          id='contactDetails__input-middle'
+          name='contactDetails.middleName'
+          onChange={props.handleChange}
+        />
+      </div>
+      <div className='contactDetails__input-ctr'>
+        <label htmlFor='contactDetails__input-last'>Last Name *</label>
+        <input
+          type='text'
+          id='contactDetails__input-last'
+          name='contactDetails.lastName'
+          onChange={props.handleChange}
+        />
+      </div>
+      <div className='contactDetails__input-ctr'>
+        <label htmlFor='contactDetails__input-email'>Email *</label>
+        <input
+          type='text'
+          id='contactDetails__input-email'
+          name='contactDetails.email'
+          onChange={props.handleChange}
+        />
+      </div>
+      <div className='contactDetails__input-ctr'>
+        <label htmlFor='contactDetails__input-phone'>Phone *</label>
+        <input
+          type='text'
+          id='contactDetails__input-phone'
+          name='contactDetails.phone'
+          onChange={props.handleChange}
+        />
+      </div>
+    </div>
   )
 }
 
