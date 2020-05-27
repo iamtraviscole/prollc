7d36f60f4eb8a84aa1664e4edb660abac423e571
Travis Cole <iamtraviscole@gmail.com>
2020-05-27 04:28:48 -0400

    add checked values

--- src/components/plans/fileState.js
+++ src/components/plans/fileState.js
@@ -5,16 +5,19 @@ import StepHeader from './stepHeader'
 import '../../styles/plans/fileState.scss'
 
 const FileState = (props) => {
+  const value = props.values.fileState
+
   return (
     <div className='fileState'>
     <StepHeader title='File State' />
       <fieldset>
         <div className='fileState__input-ctr'>
-          <input defaultChecked
+          <input
             type='radio'
             name='fileState'
             id='fileState__input-florida'
             value='Florida'
+            checked={value === 'Florida'}
             onChange={props.handleChange}
            />
           <label htmlFor='fileState__input-florida'>
@@ -27,6 +30,7 @@ const FileState = (props) => {
             name='fileState'
             id='fileState__input-delaware'
             value='Delaware'
+            checked={value === 'Delaware'}
             onChange={props.handleChange}
           />
           <label htmlFor='fileState__input-delaware'>
