567014626c111e1802d268bc7e847da64336d9cc
Travis Cole <iamtraviscole@gmail.com>
2020-05-27 04:31:51 -0400

    build out companyNames component

--- src/components/plans/companyNames.js
+++ src/components/plans/companyNames.js
@@ -1,8 +1,49 @@
 import React from 'react'
 
+import StepHeader from './stepHeader'
+
+import '../../styles/plans/companyNames.scss'
+
 const CompanyNames = (props) => {
+  const values = props.values.companyNames
+
   return (
-    <div>Company Names</div>
+    <div className='companyNames'>
+      <StepHeader
+        title='Company Names'
+        details='Order by preference'
+      />
+      <div className='companyNames__input-ctr'>
+        <label htmlFor='companyNames__input-name1'>Company Name 1 *</label>
+        <input
+          type='text'
+          id='companyNames__input-name1'
+          name='companyNames.name1'
+          onChange={props.handleChange}
+          value={values.name1}
+        />
+      </div>
+      <div className='companyNames__input-ctr'>
+        <label htmlFor='companyNames__input-name2'>Company Name 2 *</label>
+        <input
+          type='text'
+          id='companyNames__input-name2'
+          name='companyNames.name2'
+          onChange={props.handleChange}
+          value={values.name2}
+        />
+      </div>
+      <div className='companyNames__input-ctr'>
+        <label htmlFor='companyNames__input-name3'>Company Name 3 *</label>
+        <input
+          type='text'
+          id='companyNames__input-name3'
+          name='companyNames.name3'
+          onChange={props.handleChange}
+          value={values.name3}
+        />
+      </div>
+    </div>
   )
 }
 
