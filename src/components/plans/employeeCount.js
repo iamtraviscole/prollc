b70699d6ad6bd306098c00cfd81387f4004d8905
Travis Cole <iamtraviscole@gmail.com>
2020-05-28 02:08:33 -0400

    build out employeeCount component

--- src/components/plans/employeeCount.js
+++ src/components/plans/employeeCount.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import StepHeader from './stepHeader'
+
+import '../../styles/plans/employeeCount.scss'
+
 const EmployeeCount = (props) => {
   return (
-    <div>Employee Count</div>
+    <div className='employeeCount'>
+      <StepHeader
+        title='Employees'
+        details='Maximum number of employees expected in the next 12 months (enter 0 if none)'
+      />
+      <input type='number'
+        name='employeeCount'
+        id='employee__count'
+        min='0'
+        value={props.values.employeeCount}
+        onChange={props.handleChange}
+      />
+    </div>
   )
 }
 
