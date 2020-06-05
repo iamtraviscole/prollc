fdb98ebc8908a8fe7e06a0ce983d4850557cf337
Travis Cole <iamtraviscole@gmail.com>
2020-06-04 22:40:29 -0400

    display validation errors

--- src/components/plans/companyNames.js
+++ src/components/plans/companyNames.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import StepHeader from './stepHeader'
+import InputError from './inputError'
 
 import '../../styles/plans/companyNames.scss'
 
@@ -20,8 +21,10 @@ const CompanyNames = (props) => {
           id='companyNames__input-name1'
           name='companyNames.name1'
           onChange={props.handleChange}
+          onBlur={props.handleBlur}
           value={values.name1}
         />
+        <InputError objName='companyNames' valueName='name1' formik={props.formik}/>
       </div>
       <div className='companyNames__input-ctr'>
         <label htmlFor='companyNames__input-name2'>Company Name 2 *</label>
@@ -30,8 +33,10 @@ const CompanyNames = (props) => {
           id='companyNames__input-name2'
           name='companyNames.name2'
           onChange={props.handleChange}
+          onBlur={props.handleBlur}
           value={values.name2}
         />
+        <InputError objName='companyNames' valueName='name2' formik={props.formik}/>
       </div>
       <div className='companyNames__input-ctr'>
         <label htmlFor='companyNames__input-name3'>Company Name 3 *</label>
@@ -40,8 +45,10 @@ const CompanyNames = (props) => {
           id='companyNames__input-name3'
           name='companyNames.name3'
           onChange={props.handleChange}
+          onBlur={props.handleBlur}
           value={values.name3}
         />
+        <InputError objName='companyNames' valueName='name3' formik={props.formik}/>
       </div>
     </div>
   )
