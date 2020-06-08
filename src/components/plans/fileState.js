bbb893dd7f56422acdcd3cb9c1cb6e8a9a9563d9
Travis Cole <iamtraviscole@gmail.com>
2020-06-08 00:21:58 -0400

    hook up to addon prices prop

--- src/components/plans/fileState.js
+++ src/components/plans/fileState.js
@@ -6,7 +6,20 @@ import '../../styles/plans/fileState.scss'
 
 const FileState = (props) => {
   const { fileState } = props.formik.values
-  const { handleChange } = props.formik
+  const { setPrices, prices } = props.addonPrices
+
+  const handleChange = (e) => {
+    if (e.target.value === 'Delaware') {
+      setPrices({
+        ...prices, fileState: 50
+      })
+    } else {
+      setPrices({
+        ...prices, fileState: 0
+      })
+    }
+    props.formik.handleChange(e)
+  }
 
   return (
     <div className='fileState'>
@@ -19,7 +32,7 @@ const FileState = (props) => {
             id='fileState__input-florida'
             value='Florida'
             checked={fileState === 'Florida'}
-            onChange={handleChange}
+            onChange={e => handleChange(e)}
            />
           <label htmlFor='fileState__input-florida'>
             Florida
@@ -32,7 +45,7 @@ const FileState = (props) => {
             id='fileState__input-delaware'
             value='Delaware'
             checked={fileState === 'Delaware'}
-            onChange={handleChange}
+            onChange={e => handleChange(e)}
           />
           <label htmlFor='fileState__input-delaware'>
             Delaware <span>( + $50 )</span>
