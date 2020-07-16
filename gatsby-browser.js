023c16dde4363d6b36e5f38e47a7824a13a8a133
Travis Cole <iamtraviscole@gmail.com>
2020-07-16 05:02:17 -0400

    add stripe

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,5 +4,15 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+import React from 'react'
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+
 import './src/styles/global/reset.scss'
 import './src/styles/global/theme.scss'
+
+const stripePromise = loadStripe('pk_test_N38oukq70u09sWNagcxBIhdg')
+
+export const wrapRootElement = ({ element }) => (
+  <Elements stripe={stripePromise}>{element}</Elements>
+)
