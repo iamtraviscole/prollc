ff478acf9fb22393cae584a37c2f0658519ad344
Travis Cole <iamtraviscole@gmail.com>
2020-07-26 06:06:43 -0400

    build out and style Payment component

--- src/components/plans/payment.js
+++ src/components/plans/payment.js
@@ -1,8 +1,132 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { navigate } from 'gatsby'
+import axios from 'axios'
+import {
+  useStripe,
+  useElements,
+  CardNumberElement,
+  CardExpiryElement,
+  CardCvcElement
+} from '@stripe/react-stripe-js'
+
+import StepHeader from './stepHeader'
+import Loading from '../ui/loading'
+import calcPrice from '../../helpers/calcPrice.js'
+
+import '../../styles/plans/payment.scss'
+
+const STRIPE_ELEMENT_OPTIONS = {
+  style: {
+    base: {
+      fontFamily: 'Lato, sans-serif',
+      fontSmoothing: 'antialiased',
+      '::placeholder': {
+        fontWeight: '300'
+      },
+    },
+  },
+}
+
+console.log({...STRIPE_ELEMENT_OPTIONS, placeholder: 'hey'})
 
 const Payment = (props) => {
+  const { values } = props.formik
+
+  const [submitting, setSubmitting] = useState(false)
+  const [error, setError] = useState(null)
+
+  const stripe = useStripe()
+  const elements = useElements()
+
+  const handlePay = async (e) => {
+    setSubmitting(true)
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      // only need to pass one element to card, stripe automatically grabs the
+      // rest of the card information (expiry, cvc, etc.)
+      card: elements.getElement(CardNumberElement)
+      // card: elements.getElement(CardElement)
+    })
+
+    if (!error) {
+      const { id } = paymentMethod
+
+      try {
+        const res = await axios.post(
+          'http://localhost:5001/prollc/us-central1/charge',
+          {
+            id: id,
+            values: values
+          }
+        )
+        console.log('RES', res)
+
+        navigate('/success', {
+          replace: true,
+          state: {paymentId: res.id}
+        })
+      } catch (error) {
+        let { message } = error.response.data
+        setError(message)
+        setSubmitting(false)
+
+        console.error('ERR', message)
+      }
+    } else {
+      setError(error.message)
+      setSubmitting(false)
+    }
+  }
+
+  const loadingOrButton = submitting
+    ? <div className='payment__loading-ctr'><Loading /></div>
+    : <button type='button'
+        onClick={handlePay}
+        disabled={submitting}>
+        Pay and Complete Order
+      </button>
+
+  console.log(values)
+
   return (
-    <div>Payment</div>
+    <div className='payment'>
+      <StepHeader title='Review and Pay' />
+      <div className='payment__review-ctr'>
+        <h3>Contact Details</h3>
+        <p>
+          {values.contactDetails.firstName + ' ' +
+          values.contactDetails.lastName}
+        </p>
+        <p>{values.contactDetails.email}</p>
+        <p>{values.contactDetails.phone}</p>
+      </div>
+      <div className='payment__price-ctr'>
+        <h3>Total Price</h3>
+        <p>${calcPrice(values)}</p>
+      </div>
+      <div className='payment__card-ctr'>
+        <h2>Payment</h2>
+        <div className='payment__input-ctr'>
+          <p>Card Number *</p>
+          <CardNumberElement options={STRIPE_ELEMENT_OPTIONS}/>
+        </div>
+        <div className='payment__input-ctr'>
+          <p>Expiration *</p>
+          <CardExpiryElement options={{...STRIPE_ELEMENT_OPTIONS, placeholder: 'MM / YY'}}/>
+        </div>
+        <div className='payment__input-ctr'>
+          <p>CVC *</p>
+          <CardCvcElement options={{...STRIPE_ELEMENT_OPTIONS, placeholder: 'CVC'}}/>
+        </div>
+        <div className='payment__error'>
+          <p>{error && error}</p>
+        </div>
+        <div className='payment__pay-btn-ctr'>
+          {loadingOrButton}
+        </div>
+      </div>
+    </div>
   )
 }
 
