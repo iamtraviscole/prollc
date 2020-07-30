0c5d059c46268ca079eae2a575a044032a1d0cb3
Travis Cole <iamtraviscole@gmail.com>
2020-07-30 06:11:56 -0400

    build out and style

--- src/components/orders/order.js
+++ src/components/orders/order.js
@@ -51,23 +51,58 @@ const Order = (props) => {
     for (let i = 0; i < +order.members.memberCount; i++) {
 
       const memberName = memberDetails.corporateMember
-        ? <p>{memberDetails.companyName}</p>
+        ? <>
+            <div className='order__inner-header'>company name</div>
+            <p>{memberDetails.companyName}</p>
+          </>
         : <>
-            <p>{memberDetails[i].firstName}</p>
-            <p>{memberDetails[i].secondName}</p>
-            <p>{memberDetails[i].lastName}</p>
+            <div className='order__inline-group'>
+              <div className='order__inner-header'>first name</div>
+              <p>{memberDetails[i].firstName}</p>
+            </div>
+            <div className='order__inline-group'>
+              <div className='order__inner-header'>second name</div>
+              <p>{memberDetails[i].secondName || '-'}</p>
+            </div>
+            <div className='order__inline-group'>
+              <div className='order__inner-header'>last name</div>
+              <p>{memberDetails[i].lastName}</p>
+            </div>
+            <br />
           </>
 
       members.push(
-        <div key={i} className='order__details-inner-group'>
-          <p>{memberDetails[i].corporateMember}</p>
+        <div key={i} className='order__details-group'>
+          <h3>Member {i + 1}</h3>
+          <div className='order__inner-header'>corporate member</div>
+          <p>{memberDetails[i].corporateMember ? 'Yes' : 'No'}</p>
           {memberName}
-          <p>{memberDetails[i].street}</p>
-          <p>{memberDetails[i].suite}</p>
-          <p>{memberDetails[i].city}</p>
-          <p>{memberDetails[i].state}</p>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>street</div>
+            <p>{memberDetails[i].street}</p>
+          </div>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>suite</div>
+            <p>{memberDetails[i].suite || '-'}</p>
+          </div>
+          <br />
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>city</div>
+            <p>{memberDetails[i].city}</p>
+          </div>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>state</div>
+            <p>{memberDetails[i].state}</p>
+          </div>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>country</div>
+            <p>{memberDetails[i].country}</p>
+          </div>
+          <br />
+          <div className='order__inner-header'>zipcode</div>
           <p>{memberDetails[i].zipcode}</p>
-          <p>{memberDetails[i].country}</p>
+          <div className='order__inner-header'>manager</div>
+          <p>{memberDetails[i].manager ? 'Yes' : 'No'}</p>
         </div>
       )
     }
@@ -76,48 +111,57 @@ const Order = (props) => {
   }
 
   const managerDetails = () => {
-    const memberManagers = order.members.memberDetails.map((member, i) => {
-      if (member.manager) {
-        return (
-          <div key={i} className='order__details-inner-group'>
-            <p>{member.firstName}</p>
-            <p>{member.secondName}</p>
-            <p>{member.lastName}</p>
-            <p>{member.street}</p>
-            <p>{member.suite}</p>
-            <p>{member.city}</p>
-            <p>{member.state}</p>
-            <p>{member.zipcode}</p>
-            <p>{member.country}</p>
-          </div>
-        )
-      } else {
-        return 'wtf'
-      }
-    })
 
     const { managerDetails } = order.managers
 
     const managers = []
     for (let i = 0; i < +order.managers.managerCount; i++) {
       managers.push(
-        <div key={i} className='order__details-inner-group'>
-          <p>{managerDetails[i].firstName}</p>
-          <p>{managerDetails[i].secondName}</p>
-          <p>{managerDetails[i].lastName}</p>
-          <p>{managerDetails[i].street}</p>
-          <p>{managerDetails[i].suite}</p>
-          <p>{managerDetails[i].city}</p>
-          <p>{managerDetails[i].state}</p>
+        <div key={i} className='order__details-group'>
+          <h3>Additional Manager {i + 1}</h3>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>first name</div>
+            <p>{managerDetails[i].firstName}</p>
+          </div>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>second name</div>
+            <p>{managerDetails[i].secondName || '-'}</p>
+          </div>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>last name</div>
+            <p>{managerDetails[i].lastName}</p>
+          </div>
+          <br />
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>street</div>
+            <p>{managerDetails[i].street}</p>
+          </div>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>suite</div>
+            <p>{managerDetails[i].suite || '-'}</p>
+          </div>
+          <br />
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>city</div>
+            <p>{managerDetails[i].city}</p>
+          </div>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>state</div>
+            <p>{managerDetails[i].state}</p>
+          </div>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>country</div>
+            <p>{managerDetails[i].country}</p>
+          </div>
+          <br />
+          <div className='order__inner-header'>zipcode</div>
           <p>{managerDetails[i].zipcode}</p>
-          <p>{managerDetails[i].country}</p>
         </div>
       )
     }
 
     return (
       <div>
-        {memberManagers}
         {order.managers.managerCount === '0' || order.managers.managerCount === '5+'
           ? ''
           : managers}
@@ -126,164 +170,244 @@ const Order = (props) => {
   }
 
   return (
-   <div className='order'>
-     <div className='order__link-ctr'>
-       <Link to='/orders' className='all-orders-link'>Search Orders</Link>
-     </div>
-     <div className='order__order-ctr'>
-       <div className='order__details-group order-id'>
-         <h1>ORDER ID: <span>{order.id}</span></h1>
-         <p>
-           {
-             order.paymentTime
-             ? formatDate(new Date(order.paymentTime * 1000))
-             : ''
-           }
-         </p>
-       </div>
+    <div className='order'>
+    <div className='order__order-ctr'>
+      <div className='order__date'>
+        {order.paymentTime
+          ? formatDate(new Date(order.paymentTime * 1000))
+          : ''
+        }
+      </div>
 
-       <div className='order__details-group'>
-         <h3>Plan</h3>
-         <p>{order.plan}</p>
-       </div>
+      <div className='order__top-ctr'>
+        <div className='order__top-inline-group'>
+          <h1>ORDER ID</h1>
+          <p>{order.id}</p>
+        </div>
+        <div className='order__top-inline-group'>
+          <h1>PAYMENT ID</h1>
+          <p>{order.paymentId}</p>
+        </div>
+      </div>
 
-       <div className='order__details-group'>
-         <h3>File State</h3>
-         <p>{order.fileState}</p>
-       </div>
+      <div className='order__details-group'>
+        <h3>Plan</h3>
+        <p>{order.plan}</p>
+      </div>
 
-       <div className='order__details-group'>
-         <h3>Contact Details</h3>
-         <p>First Name: {order.contactDetails.firstName}</p>
-         <p>Second Name: {order.contactDetails.secondName}</p>
-         <p>{order.contactDetails.lastName}</p>
-         <p>{order.contactDetails.email}</p>
-         <p>{order.contactDetails.phone}</p>
-       </div>
+      <div className='order__details-group'>
+        <h3>Price</h3>
+        <p>${order.price}</p>
+      </div>
 
-       <div className='order__details-group'>
-         <h3>Company Names</h3>
-         <p>{order.companyNames.name1}</p>
-         <p>{order.companyNames.name2}</p>
-         <p>{order.companyNames.name3}</p>
-       </div>
+      <div className='order__details-group'>
+        <h3>File State</h3>
+        <p>{order.fileState}</p>
+      </div>
 
-       <div className='order__details-group'>
-         <h3>Denomination</h3>
-         <p>{order.denomination}</p>
-       </div>
+      <div className='order__details-group'>
+        <h3>Contact Details</h3>
+        <div className='order__inline-group'>
+          <div className='order__inner-header'>first name</div>
+          <p>{order.contactDetails.firstName}</p>
+        </div>
+        <div className='order__inline-group'>
+          <div className='order__inner-header'>second name</div>
+          <p>{order.contactDetails.secondName || '-'}</p>
+        </div>
+        <div className='order__inline-group'>
+          <div className='order__inner-header'>last name</div>
+          <p>{order.contactDetails.lastName}</p>
+        </div>
+        <div className='order__inner-header'>email</div>
+        <p>{order.contactDetails.email}</p>
+        <div className='order__inner-header'>phone</div>
+        <p>{order.contactDetails.phone}</p>
+      </div>
 
-       <div className='order__details-group'>
-         <h3>Industry</h3>
-         {order.industry.industry === 'Other'
-           ? <p>{order.industry.other}</p>
-           : <p>{order.industry.industry}</p>
-         }
-       </div>
+      <div className='order__details-group'>
+        <h3>Company Names</h3>
+        <div className='order__inner-header'>first choice</div>
+        <p>{order.companyNames.name1}</p>
+        <div className='order__inner-header'>second choice</div>
+        <p>{order.companyNames.name2}</p>
+        <div className='order__inner-header'>third choice</div>
+        <p>{order.companyNames.name3}</p>
+      </div>
 
-       <div className='order__details-group'>
-         <h3>Employee Count</h3>
-         <p>{order.employeeCount}</p>
-       </div>
+      <div className='order__details-group'>
+        <h3>Denomination</h3>
+        <p>{order.denomination}</p>
+      </div>
 
-       <div className='order__details-group'>
-         <h3>Address</h3>
-         <p>{order.companyAddress.proAddress}</p>
-         <br />
-         {order.companyAddress.proAddress === 'No' &&
-           <>
-             <p>{order.companyAddress.street}</p>
-             <p>{order.companyAddress.suite}</p>
-             <p>{order.companyAddress.city}</p>
-             <p>{order.companyAddress.state}</p>
-             <p>{order.companyAddress.zipcode}</p>
-             <p>{order.companyAddress.country}</p>
-           </>
-         }
-       </div>
+      <div className='order__details-group'>
+        <h3>Industry</h3>
+        {order.industry.industry === 'Other'
+          ? <p>{order.industry.other}</p>
+          : <p>{order.industry.industry}</p>
+        }
+      </div>
 
-       <div className='order__details-group'>
-         <h3>Members</h3>
-         <p>{order.members.memberCount}</p>
-         <br />
-         <div>{memberDetails()}</div>
-       </div>
+      <div className='order__details-group'>
+        <h3>Employee Count</h3>
+        <p>{order.employeeCount}</p>
+      </div>
 
-       <div className='order__details-group'>
-         <h3>Managers</h3>
-         <p>{order.managers.managerCount}</p>
-         <br />
-         <div>{managerDetails()}</div>
-       </div>
+      <div className='order__details-group'>
+        <h3>Pro Address</h3>
+        <p>{order.companyAddress.proAddress}</p>
+      </div>
 
-       <div className='order__details-group'>
-         <h3>Registered Agent</h3>
-         <p>{order.registeredAgent.proRegisteredAgent}</p>
-         <br />
-         {order.registeredAgent.proRegisteredAgent === 'No' &&
-           <div className='order__details-inner-group'>
-             {order.registeredAgent.corporateRegisteredAgent
-               ? <p>{order.registeredAgent.companyName}</p>
-               : <>
-                   <p>{order.registeredAgent.firstName}</p>
-                   <p>{order.registeredAgent.secondName}</p>
-                   <p>{order.registeredAgent.lastName}</p>
-                 </>}
-             <p>{order.registeredAgent.street}</p>
-             <p>{order.registeredAgent.suite}</p>
-             <p>{order.registeredAgent.city}</p>
-             <p>{order.registeredAgent.state}</p>
-             <p>{order.registeredAgent.zipcode}</p>
-             <p>{order.registeredAgent.country}</p>
-           </div>
-         }
-       </div>
+      {order.companyAddress.proAddress === 'No' &&
+        <div className='order__details-group'>
+          <h3>Address</h3>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>street</div>
+            <p>{order.companyAddress.street}</p>
+          </div>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>suite</div>
+            <p>{order.companyAddress.suite}</p>
+          </div>
+          <br />
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>city</div>
+            <p>{order.companyAddress.city}</p>
+          </div>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>state</div>
+            <p>{order.companyAddress.state}</p>
+          </div>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>country</div>
+            <p>{order.companyAddress.country}</p>
+          </div>
+          <br />
+          <div className='order__inner-header'>zipcode</div>
+          <p>{order.companyAddress.zipcode}</p>
+        </div>
+      }
 
-       {(order.plan === 'Complete' || order.plan === 'Pro') &&
-         <>
-           <div className='order__details-group'>
-             <h3>EIN</h3>
-             <p>{order.ein.firstName}</p>
-             <p>{order.ein.secondName}</p>
-             <p>{order.ein.lastName}</p>
-             <p>{order.ein.ssn}</p>
-             <p>{order.ein.itin}</p>
-           </div>
+      <div className='order__details-group'>
+        <h3>Members</h3>
+        <p>{order.members.memberCount}</p>
+      </div>
 
-           <div className='order__details-group'>
-             <h3>S-Election</h3>
-             <p>{order.sElection}</p>
-           </div>
-         </>
-       }
+      {memberDetails()}
 
-       {order.plan === 'Pro' &&
-         <>
-           <div className='order__details-group'>
-             <h3>Banking</h3>
-             <p>{order.banking.presentInUS}</p>
-             <p>{order.banking.bank}</p>
-             <p>{order.banking.bankOther}</p>
-             <p>{order.banking.otherOptions}</p>
-           </div>
+      <div className='order__details-group'>
+        <h3>Additional Managers</h3>
+        <p>{order.managers.managerCount}</p>
+      </div>
 
-           <div className='order__details-group'>
-             <h3>Speak To Attorney</h3>
-             <p>{order.speakToAttorney}</p>
-           </div>
-         </>
-       }
+      {managerDetails()}
 
-       <div className='order__details-group'>
-         <h3>Expedited</h3>
-         <p>{order.expedited}</p>
-       </div>
+      <div className='order__details-group'>
+        <h3>Pro Registered Agent</h3>
+        <p>{order.registeredAgent.proRegisteredAgent}</p>
+      </div>
 
-       <div className='order__details-group'>
-         <h3>Payment Details</h3>
-         <p>{order.paymentId}</p>
-         <p>${order.price}</p>
-       </div>
+      {order.registeredAgent.proRegisteredAgent === 'No' &&
+        <div className='order__details-group'>
+          <h3>Registered Agent Details</h3>
+          {order.registeredAgent.corporateRegisteredAgent
+            ? <>
+                <div className='order__inner-header'>company name</div>
+                <p>{order.registeredAgent.companyName}</p>
+              </>
+            : <>
+                <div className='order__inline-group'>
+                  <div className='order__inner-header'>first name</div>
+                  <p>{order.registeredAgent.firstName}</p>
+                </div>
+                <div className='order__inline-group'>
+                  <div className='order__inner-header'>second name</div>
+                  <p>{order.registeredAgent.secondName}</p>
+                </div>
+                <div className='order__inline-group'>
+                  <div className='order__inner-header'>last name</div>
+                  <p>{order.registeredAgent.lastName}</p>
+                </div>
+              </>
+          }
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>street</div>
+            <p>{order.registeredAgent.street}</p>
+          </div>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>suite</div>
+            <p>{order.registeredAgent.suite || '-'}</p>
+          </div>
+          <br />
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>city</div>
+            <p>{order.registeredAgent.city}</p>
+          </div>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>state</div>
+            <p>{order.registeredAgent.state}</p>
+          </div>
+          <div className='order__inline-group'>
+            <div className='order__inner-header'>country</div>
+            <p>{order.registeredAgent.country}</p>
+          </div>
+          <br />
+          <div className='order__inner-header'>zipcode</div>
+          <p>{order.registeredAgent.zipcode}</p>
+        </div>
+      }
+
+      {(order.plan === 'Complete' || order.plan === 'Pro') &&
+        <>
+          <div className='order__details-group'>
+            <h3>EIN</h3>
+            <div className='order__inline-group'>
+              <div className='order__inner-header'>first name</div>
+              <p>{order.ein.firstName}</p>
+            </div>
+            <div className='order__inline-group'>
+              <div className='order__inner-header'>second name</div>
+              <p>{order.ein.secondName || '-'}</p>
+            </div>
+            <div className='order__inline-group'>
+              <div className='order__inner-header'>last name</div>
+              <p>{order.ein.lastName}</p>
+            </div>
+            <div className='order__inner-header'>ssn</div>
+            <p>{order.ein.ssn}</p>
+            <div className='order__inner-header'>itin</div>
+            <p>{order.ein.itin}</p>
+          </div>
+
+          <div className='order__details-group'>
+            <h3>S-Election</h3>
+            <p>{order.sElection}</p>
+          </div>
+        </>
+      }
+
+      {order.plan === 'Pro' &&
+        <>
+          <div className='order__details-group'>
+            <h3>Banking</h3>
+            <p>{order.banking.presentInUS}</p>
+            <p>{order.banking.bank}</p>
+            <p>{order.banking.bankOther}</p>
+            <p>{order.banking.otherOptions}</p>
+          </div>
+
+          <div className='order__details-group'>
+            <h3>Speak To Attorney</h3>
+            <p>{order.speakToAttorney}</p>
+          </div>
+        </>
+      }
+
+      <div className='order__details-group'>
+        <h3>Expedited</h3>
+        <p>{order.expedited}</p>
+      </div>
+
      </div>
    </div>
   )
