5586f5e80692b96420cdd6f55278a57436a7a65d
Travis Cole <iamtraviscole@gmail.com>
2020-07-30 19:33:46 -0400

    create services component

---
+++ src/pages/services.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+import '../styles/services.scss'
+
+const Services = (props) => {
+  return (
+    <Layout pageTitle='Services'>
+      <SEO title='Services' />
+      <div className='services'>
+      </div>
+    </Layout>
+  )
+}
+
+export default Services
