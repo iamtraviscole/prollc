d748a80a6a5733ac15d2f5c00265658b60b382ae
Travis Cole <iamtraviscole@gmail.com>
2020-07-30 19:46:57 -0400

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,10 +3,13 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import '../styles/404.scss'
+
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
+  <Layout pageTitle='404'>
+    <div className='pageNotFound'>
+      <h1>Page not found</h1>
+    </div>
   </Layout>
 )
 
