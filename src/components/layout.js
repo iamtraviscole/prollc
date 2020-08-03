10f42e6bcc7ad95abf2fb75eb6a6a269de1cd8cc
Travis Cole <iamtraviscole@gmail.com>
2020-08-02 21:22:13 -0400

    add favicons

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { Helmet } from "react-helmet"
 
 import Header from './header'
+import favicon16 from "../images/favicon-16.png";
+import favicon32 from "../images/favicon-32.png";
+
 import '../styles/layout.scss'
 
 const Layout = (props) => {
@@ -9,8 +13,15 @@ const Layout = (props) => {
       <h1>{props.pageTitle}</h1>
     </div>
   )
+
+  const faviconLinks = [
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: favicon16 },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: favicon32 },
+  ];
+
   return (
     <div className='layout'>
+      <Helmet link={faviconLinks} />
       <Header />
       {subheader}
       <main className='layout__main'>{props.children}</main>
