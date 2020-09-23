6ef646e307fe1e7e969fecf8fe79e094bddeae4b
Travis Cole <iamtraviscole@gmail.com>
2020-09-23 04:51:40 -0400

    add services

--- src/pages/services.js
+++ src/pages/services.js
@@ -10,6 +10,26 @@ const Services = (props) => {
     <Layout pageTitle='Servicios'>
       <SEO title='Servicios' />
       <div className='services'>
+        <ul>
+          <li>Planes</li>
+          <li>Agente Registrado</li>
+          <li>Conversión</li>
+          <li>Reporte Anual</li>
+          <li>Enmiendas</li>
+          <li>Servicios de domicilio</li>
+          <li>Servicios nominales</li>
+          <li>Disolución</li>
+          <li>Reactivación</li>
+          <li>DBA (nombre ficticio)</li>
+          <li>Registro de compañías extranjeras</li>
+          <li>Destitución</li>
+          <li>Declaraciones de Autoridad</li>
+          <li>Certificado de Estatus</li>
+          <li>Copias Certificadas</li>
+          <li>Cambio de datos de la sociedad</li>
+          <li>Cuenta Bancaria</li>
+          <li>Apostilla</li>
+        </ul>
       </div>
     </Layout>
   )
