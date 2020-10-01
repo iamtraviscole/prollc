07a3d86f335e401fde172b4b4db0bf314044852e
Travis Cole <iamtraviscole@gmail.com>
2020-10-01 04:06:01 -0400

    build out order success page

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { navigate, Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+import '../styles/success.scss'
 
 const Success = (props) => {
-  console.log(props)
+
+  useEffect(() => {
+    console.log('hello')
+    if (!props.location.state || !props.location.state.paymentId) {
+      navigate('/')
+    }
+  }, [])
+
+
   return (
-    <div>
-      Thanks for your order!
-    </div>
+    <Layout pageTitle='¡Éxito!'>
+      <div className='success'>
+        <div className='success__ctr'>
+          <h1>¡Gracias por su orden!</h1>
+          <p>Nos pondremos en contacto con usted pronto</p>
+          <br />
+          <div className='success__contact-ctr'>
+            Si tiene alguna pregunta, por favor <Link to='/contact'>contáctenos</Link>
+          </div>
+        </div>
+      </div>
+    </Layout>
   )
 }
 
