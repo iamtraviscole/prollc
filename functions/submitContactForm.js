e934f9630fb3dee6d4c72f3a906c87d407504b40
Travis Cole <iamtraviscole@gmail.com>
2020-09-30 23:54:58 -0400

    add function for contact form submission

---
+++ functions/submitContactForm.js
@@ -0,0 +1,24 @@
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_KEY)
+
+exports.handler = async (event, context, callback) => {
+  const { name, email, message } = JSON.parse(event.body)
+
+  const msg = {
+    to: email,
+    from: 'iamtraviscole@gmail.com', // make this an .env var?
+    subject: 'ProLLC Contact Form Submission',
+    html: `<p><b>Name:</b> ${name || ''}</p>
+    <p><b>Email:</b> ${email}
+    <p style="white-space: pre-wrap;"><b>Message:</b><br>${message}</p>`
+  }
+
+  try {
+    await sgMail.send(msg)
+    return {statusCode: 200, body: 'Success'}
+  } catch (error) {
+    console.error(error)
+    return {statusCode: 200, body: 'Something went wrong'}
+  }
+}
