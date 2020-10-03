612367063470627b1e22a4cdf3629cf94b8e0d6e
Travis Cole <iamtraviscole@gmail.com>
2020-10-03 19:35:54 -0400

    add social media to footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Helmet } from "react-helmet"
+import { Helmet } from 'react-helmet'
 
 import Header from './header'
-import favicon16 from "../images/favicon-16.png";
-import favicon32 from "../images/favicon-32.png";
+import favicon16 from '../images/favicon-16.png'
+import favicon32 from '../images/favicon-32.png'
+import fbIcon from '../images/fb-icon.png'
+import igIcon from '../images/ig-icon.png'
+import whatsAppIcon from '../images/whatsapp-icon.png'
 
 import '../styles/layout.scss'
 
@@ -26,6 +29,20 @@ const Layout = (props) => {
       {subheader}
       <main className='layout__main'>{props.children}</main>
       <footer className='layout__footer'>
+        <div className='layout__social'>
+          <a href='http://www.facebook.com/ProLLC' target='_blank' rel='noreferrer noopener'>
+            <img src={fbIcon} width='30' alt='Facebook Icon' />
+          </a>
+          <a href='http://www.instagram.com/pro.llc' target='_blank' rel='noreferrer noopener'>
+            <img src={igIcon} width='30' alt='Instagram Icon' />
+          </a>
+        </div>
+        <div className='layout__whatsApp-outer-ctr'>
+          <div className='layout__whatsApp-inner-ctr'>
+            <img src={whatsAppIcon} height='25' alt='WhatsApp Icon'></img>
+            <p>1.305.906.0973</p>
+          </div>
+        </div>
         <div className='layout__copyright'>
           ProLLC © {new Date().getFullYear()}
         </div>
