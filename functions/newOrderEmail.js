e0ebbbeffca31df0296d077acec1cc2a95bc6660
Travis Cole <iamtraviscole@gmail.com>
2020-10-05 04:42:13 -0400

    create order notification function

---
+++ functions/newOrderEmail.js
@@ -0,0 +1,27 @@
+const sgMail = require('@sendgrid/mail')
+
+sgMail.setApiKey(process.env.SENDGRID_KEY)
+
+exports.handler = async (event, context, callback) => {
+  const { firstName, lastName, email, price, paymentId } = JSON.parse(event.body)
+
+  const msg = {
+    to: process.env.PROLLC_ORDERS_EMAIL,
+    from: process.env.PROLLC_ORDERS_EMAIL,
+    subject: 'ProLLC - New Order: ' + paymentId,
+    html: `<p><b>Name:</b> ${firstName + ' ' + lastName}</p>
+    <p><b>Email:</b> ${email}
+    <p><b>Payment ID:</b> ${paymentId}
+    <br>
+    <br>
+    <a href='prollc.com/orders/${paymentId}'>prollc.com/orders/${paymentId}</a>`
+  }
+
+  try {
+    await sgMail.send(msg)
+    return {statusCode: 200, body: 'Success'}
+  } catch (error) {
+    console.error(error)
+    return {statusCode: 200, body: 'Something went wrong'}
+  }
+}
