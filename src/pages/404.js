6d6a3158bcad84b6ddbe19657d83b546489ada24
Travis Cole <iamtraviscole@gmail.com>
2020-10-09 02:42:58 -0400

    stop 404 page from flashing on orders routes

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,12 +4,16 @@ import Layout from "../components/layout"
 
 import '../styles/404.scss'
 
+const browser = typeof window !== 'undefined' && window
+
 const NotFoundPage = () => (
-  <Layout pageTitle='404'>
-    <div className='pageNotFound'>
-      <h1>Page not found</h1>
-    </div>
-  </Layout>
+  browser && (
+    <Layout pageTitle='404'>
+      <div className='pageNotFound'>
+        <h1>Page not found</h1>
+      </div>
+    </Layout>
+  )
 )
 
 export default NotFoundPage
