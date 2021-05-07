08f4476c67ac8603afa67c54a38ae9ddc217609e
Travis Cole <iamtraviscole@gmail.com>
2021-05-07 13:26:57 -0400

    disable eslint warning

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/media-has-caption */
+
 import React from 'react'
 import { useStaticQuery, graphql, Link } from 'gatsby'
 import BackgroundImage from 'gatsby-background-image'
